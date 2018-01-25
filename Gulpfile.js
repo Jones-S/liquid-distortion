9bbe3c628929be89394ea702da5cf3eff75ce53b
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2018-01-25 12:21:31 +0100

    [Update] Add gitignore, editorconfig and add gulp task to copy html files to dist folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,18 @@
 "use strict";
 
 var gulp = require("gulp"),
-		browserSync = require("browser-sync"),
-		sass = require("gulp-sass"),
-		bourbon = require("node-bourbon").includePaths,
-		neat = require("node-neat").includePaths;
+	browserSync = require("browser-sync"),
+	sass = require("gulp-sass"),
+	bourbon = require("node-bourbon").includePaths,
+	neat = require("node-neat").includePaths;
 
 // Compiles all gulp tasks
 gulp.task("default", ["sass"]);
 
 // Live reload anytime a file changes
-gulp.task("watch", ["browserSync", "sass"], function() {
-	gulp.watch("src/scss/**/*.scss", ["sass"]);
-	gulp.watch("dist/*.html").on("change", browserSync.reload);
+gulp.task("watch", ["browserSync", "sass", "copy_html"], function() {
+  gulp.watch("src/scss/**/*.scss", ["sass"]);
+	gulp.watch("src/**/*.html", ["copy_html"]);
 });
 
 // Spin up a server
@@ -32,6 +32,15 @@ gulp.task("sass", function() {
 				includePaths: neat
 			}))
 			.pipe(gulp.dest("dist/css"))
+			.pipe(browserSync.reload({
+				stream: true
+			}))
+});
+
+// Copy html files to dist directory
+gulp.task("copy_html", function() {
+	gulp.src("src/**/*.html")
+			.pipe(gulp.dest("dist"))
 			.pipe(browserSync.reload({
 				stream: true
 			}))
